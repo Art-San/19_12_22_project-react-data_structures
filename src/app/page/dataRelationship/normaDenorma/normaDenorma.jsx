2d23cfc4172de9b77e8595f4e5c9acea2501c90a
Aleksander <asava@sibmail.com>
2022-12-20 17:20:16 +0700

    добав. стр. норм_денорм

--- src/app/page/dataRelationship/normaDenorma/normaDenorma.jsx
+++ src/app/page/dataRelationship/normaDenorma/normaDenorma.jsx
@@ -18,6 +18,8 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import diagram1 from 'app/assets/img/diagrams/DeNormalized.jpg'
 import diagram2 from 'app/assets/img/diagrams/Normalized.jpg'
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 const NormaDenorma = () => {
     return (
         <>
@@ -79,114 +81,137 @@ const NormaDenorma = () => {
                 в качестве структуры данных при ответах с сервера по API.
             </Text>
             <SmallTitle>Денормализация</SmallTitle>
-            <Text>
-                Денормализация — это метод, который используется при проектировании в базе данных.
-                Он используется для добавления избыточности для быстрого выполнения запроса.
-                Этот метод, при котором данные объединяются. Он обладает характеристиками:
-            </Text>
-            <SmallTitle>Характеристики</SmallTitle>
-            <Stack
-                direction="row"
-                spacing={1}
-                style={{ margin: '10px 0 20px' }}
-            >
-                <Chip
-                    label="Скорость обработки запроса"
-                    variant="outlined"
-                    icon={<KeyboardArrowUpIcon style={{ color: 'green' }} />}
-                />
-                <Chip
-                    label="Количество сущностей"
-                    variant="outlined"
-                    icon={<KeyboardArrowDownIcon style={{ color: 'green' }} />}
-                />
-                <Chip
-                    label="Избыточность данных"
-                    variant="outlined"
-                    icon={<KeyboardArrowUpIcon style={{ color: 'red' }} />}
-                />
-                <Chip
-                    label="Согласованность данных"
-                    variant="outlined"
-                    icon={<KeyboardArrowDownIcon style={{ color: 'red' }} />}
-                />
-            </Stack>
-            <SmallTitle>Пример</SmallTitle>
-            <CodeBlock
-                text={exampleDenormalization}
-                language="js"
-                theme={dracula}
-                wrapLines={true}
-            />
-            <Text>
-                У нас есть заказ, у которого есть способ доставки,
-                адрес доставки и прочие данные. Обычно служба доставки
-                и адрес — это данные, которые являются справочными
-                и редко меняются. Но если мы хотим отобразить конкретный
-                оформленный заказ конкретному пользователю, то ему нужна
-                только информация о его заказе и не нужны все существующие
-                варианты адресов и доставок. Поэтому мы будем запрашивать
-                из хранилища данных только адрес в этом заказе, т. е.
-                денормализованные данные. В данном случае денормализация
-                совершенно оправдана.
-            </Text>
-            <br/>
-            <Text>
-                Но что, если бы мы работали от лица, например, аналитика в интернет-магазине,
-                и мы бы хотели получить список заказов, у которых одна и та же служба доставки?
-                Нам совершенно не нужно было бы хранить множество раз одну и ту же доставку в сущности заказа.
-                Для решения такой задача к нам приходит второй метод проектирования структуры данных — нормализация.
-            </Text>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>внутри больше. ЖМИ!</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Денормализация — это метод, который используется при проектировании в базе данных.
+                        Он используется для добавления избыточности для быстрого выполнения запроса.
+                        Этот метод, при котором данные объединяются. Он обладает характеристиками:
+                    </Typography>
+                    <SmallTitle>Характеристики</SmallTitle>
+                    <Stack
+                        direction="row"
+                        spacing={1}
+                        style={{ margin: '10px 0 20px' }}
+                    >
+                        <Chip
+                            label="Скорость обработки запроса"
+                            variant="outlined"
+                            icon={<KeyboardArrowUpIcon style={{ color: 'green' }} />}
+                        />
+                        <Chip
+                            label="Количество сущностей"
+                            variant="outlined"
+                            icon={<KeyboardArrowDownIcon style={{ color: 'green' }} />}
+                        />
+                        <Chip
+                            label="Избыточность данных"
+                            variant="outlined"
+                            icon={<KeyboardArrowUpIcon style={{ color: 'red' }} />}
+                        />
+                        <Chip
+                            label="Согласованность данных"
+                            variant="outlined"
+                            icon={<KeyboardArrowDownIcon style={{ color: 'red' }} />}
+                        />
+                    </Stack>
+                    <SmallTitle>Пример</SmallTitle>
+                    <CodeBlock
+                        text={exampleDenormalization}
+                        language="js"
+                        theme={dracula}
+                        wrapLines={true}
+                    />
+                    <Text>
+                        У нас есть заказ, у которого есть способ доставки,
+                        адрес доставки и прочие данные. Обычно служба доставки
+                        и адрес — это данные, которые являются справочными
+                        и редко меняются. Но если мы хотим отобразить конкретный
+                        оформленный заказ конкретному пользователю, то ему нужна
+                        только информация о его заказе и не нужны все существующие
+                        варианты адресов и доставок. Поэтому мы будем запрашивать
+                        из хранилища данных только адрес в этом заказе, т. е.
+                        денормализованные данные. В данном случае денормализация
+                        совершенно оправдана.
+                    </Text>
+                    <br/>
+                    <Text>
+                        Но что, если бы мы работали от лица, например, аналитика в интернет-магазине,
+                        и мы бы хотели получить список заказов, у которых одна и та же служба доставки?
+                        Нам совершенно не нужно было бы хранить множество раз одну и ту же доставку в сущности заказа.
+                        Для решения такой задача к нам приходит второй метод проектирования структуры данных — нормализация.
+                    </Text>
+                </AccordionDetails>
+            </Accordion>
             <SmallTitle>Нормализация</SmallTitle>
-            <Text>Нормализация — это метод, используемый в базе данных
-                для уменьшения избыточности и несогласованности данных.
-                Это метод, при котором данные без избыточности
-                и непротиворечивости хранятся в установленной схеме.
-            </Text>
-            <SmallTitle>Характеристики</SmallTitle>
-            <Stack
-                direction="row"
-                spacing={1}
-                style={{ margin: '10px 0 20px' }}
-            >
-                <Chip
-                    label="Скорость обработки запроса"
-                    variant="outlined"
-                    icon={<KeyboardArrowDownIcon style={{ color: 'red' }} />}
-                />
-                <Chip
-                    label="Количество сущностей"
-                    variant="outlined"
-                    icon={<KeyboardArrowUpIcon style={{ color: 'red' }} />}
-                />
-                <Chip
-                    label="Избыточность данных"
-                    variant="outlined"
-                    icon={<KeyboardArrowDownIcon style={{ color: 'green' }} />}
-                />
-                <Chip
-                    label="Согласованность данных"
-                    variant="outlined"
-                    icon={<KeyboardArrowUpIcon style={{ color: 'green' }} />}
-                />
-            </Stack>
-            <SmallTitle>Пример</SmallTitle>
-            <CodeBlock
-                text={exampleNormalization}
-                language="js"
-                theme={dracula}
-                wrapLines={true}
-            />
-            <Text>
-                Мы видим, что у нас заказы и службы доставки разделились
-                на разные сущности: заказ и доставка.
-            </Text>
-            <br/>
-            <Text>
-                Сущность “заказ” имеет указатель в виде shipmentTypeId на службу доставки.
-                Если бы такой запрос обрабатывался на сервере, то сначала были бы запрошены заказы,
-                собраны идентификаторы служб доставок, и был бы выполнен ещё один запрос за службами доставки.
-            </Text>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>внутри больше. ЖМИ!</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Нормализация — это метод, используемый в базе данных
+                        для уменьшения избыточности и несогласованности данных.
+                        Это метод, при котором данные без избыточности
+                        и непротиворечивости хранятся в установленной схеме.
+                    </Typography>
+                    <SmallTitle>Характеристики</SmallTitle>
+                    <Stack
+                        direction="row"
+                        spacing={1}
+                        style={{ margin: '10px 0 20px' }}
+                    >
+                        <Chip
+                            label="Скорость обработки запроса"
+                            variant="outlined"
+                            icon={<KeyboardArrowDownIcon style={{ color: 'red' }} />}
+                        />
+                        <Chip
+                            label="Количество сущностей"
+                            variant="outlined"
+                            icon={<KeyboardArrowUpIcon style={{ color: 'red' }} />}
+                        />
+                        <Chip
+                            label="Избыточность данных"
+                            variant="outlined"
+                            icon={<KeyboardArrowDownIcon style={{ color: 'green' }} />}
+                        />
+                        <Chip
+                            label="Согласованность данных"
+                            variant="outlined"
+                            icon={<KeyboardArrowUpIcon style={{ color: 'green' }} />}
+                        />
+                    </Stack>
+                    <SmallTitle>Пример</SmallTitle>
+                    <CodeBlock
+                        text={exampleNormalization}
+                        language="js"
+                        theme={dracula}
+                        wrapLines={true}
+                    />
+                    <Text>
+                        Мы видим, что у нас заказы и службы доставки разделились
+                        на разные сущности: заказ и доставка.
+                    </Text>
+                    <br/>
+                    <Text>
+                        Сущность “заказ” имеет указатель в виде shipmentTypeId на службу доставки.
+                        Если бы такой запрос обрабатывался на сервере, то сначала были бы запрошены заказы,
+                        собраны идентификаторы служб доставок, и был бы выполнен ещё один запрос за службами доставки.
+                    </Text>
+                </AccordionDetails>
+            </Accordion>
             <br/>
             <Text>
                 Для наглядности можно представить схематически,
@@ -215,64 +240,75 @@ const NormaDenorma = () => {
             </Text>
             <br/>
             <SmallTitle>Спойлер</SmallTitle>
-            <Text>
-                Возможно ты знаком с реляционными базами данных (например MySQL).
-                При проектировании таких баз данных используют нормализацию.
-                Однако часто нам нужно быстро получать денормализованные данные.
-                Например у нас есть страница списка товаров (в интернет-магазине),
-                в котором данные запрашиваются из разных таблиц БД, и при этом
-                у нас происходят тысячи запросов. В Backend для решения таких задач
-                используют кэш или другую базу данных, например Redis, просто запомнив
-                для определённого id сущности результат запроса из базы данных.
-            </Text>
-            <br/>
-            <Text>
-                Если наше хранилище данных в React использует нормализацию (как, например,
-                советует Redux), то мы можем подобным способом,
-                как на Backend, проводить денормализацию и “кэшировать” с помощью мемоизации.
-                В React мемоизацию в данном контексте можно назвать кэшированием. Например,
-                у нас существует кабинет менеджера интернет-магазина. У него есть хранилище,
-                в котором есть справочники с доступными скидками:
-            </Text>
-            <br/>
-            <CodeBlock
-                text={discount}
-                language="js"
-                theme={dracula}
-                wrapLines={true}
-            />
-            <br/>
-            <Text>
-                В какой-то момент этот менеджер начинает работать
-                с заказом какого-то пользователя, у которого есть скидка:
-            </Text>
-            <br/>
-            <CodeBlock
-                text={discountUser}
-                language="js"
-                theme={dracula}
-                wrapLines={true}
-            />
-            <br/>
-            <Text>
-                При отображении в кабинете мы хотим показывать менеджеру скидку
-                в виде текста, и при этом не хотим каждый раз проходиться
-                по массиву словаря скидок, чтобы найти нужное значение.
-                Мы можем мемоизировать данные, которые нам нужны для отображения в кабинете:
-            </Text>
-            <br/>
-            <CodeBlock
-                text={memoizeData}
-                language="js"
-                theme={dracula}
-                wrapLines={true}
-            />
-            <br/>
-            <Text>
-                В переменной pageData мы получили мемоизированный объект
-                с денормализованными данными о заказе
-                (вместо discountId будет сразу discountTitle):
-            </Text>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>внутри больше. ЖМИ!</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Text>
+                        Возможно ты знаком с реляционными базами данных (например MySQL).
+                        При проектировании таких баз данных используют нормализацию.
+                        Однако часто нам нужно быстро получать денормализованные данные.
+                        Например у нас есть страница списка товаров (в интернет-магазине),
+                        в котором данные запрашиваются из разных таблиц БД, и при этом
+                        у нас происходят тысячи запросов. В Backend для решения таких задач
+                        используют кэш или другую базу данных, например Redis, просто запомнив
+                        для определённого id сущности результат запроса из базы данных.
+                    </Text>
+                    <br/>
+                    <Text>
+                        Если наше хранилище данных в React использует нормализацию (как, например,
+                        советует Redux), то мы можем подобным способом,
+                        как на Backend, проводить денормализацию и “кэшировать” с помощью мемоизации.
+                        В React мемоизацию в данном контексте можно назвать кэшированием. Например,
+                        у нас существует кабинет менеджера интернет-магазина. У него есть хранилище,
+                        в котором есть справочники с доступными скидками:
+                    </Text>
+                    <br/>
+                    <CodeBlock
+                        text={discount}
+                        language="js"
+                        theme={dracula}
+                        wrapLines={true}
+                    />
+                    <br/>
+                    <Text>
+                        В какой-то момент этот менеджер начинает работать
+                        с заказом какого-то пользователя, у которого есть скидка:
+                    </Text>
+                    <br/>
+                    <CodeBlock
+                        text={discountUser}
+                        language="js"
+                        theme={dracula}
+                        wrapLines={true}
+                    />
+                    <br/>
+                    <Text>
+                        При отображении в кабинете мы хотим показывать менеджеру скидку
+                        в виде текста, и при этом не хотим каждый раз проходиться
+                        по массиву словаря скидок, чтобы найти нужное значение.
+                        Мы можем мемоизировать данные, которые нам нужны для отображения в кабинете:
+                    </Text>
+                    <br/>
+                    <CodeBlock
+                        text={memoizeData}
+                        language="js"
+                        theme={dracula}
+                        wrapLines={true}
+                    />
+                    <br/>
+                    <Text>
+                        В переменной pageData мы получили мемоизированный объект
+                        с денормализованными данными о заказе
+                        (вместо discountId будет сразу discountTitle):
+                    </Text>
+                </AccordionDetails>
+            </Accordion>
 
         </>
     )
